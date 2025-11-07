9d8f59f5061136076d7001482e18655fb57a0304
Sabelli <sabellinho@gmail.com>
2025-11-07 20:12:06 -0300

    style(Error): adicionando estilização

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 
-export default function Error(){
-    return(
-        <div>
-            <h1>Error</h1>
-            <Link to="/">Home</Link>
-        </div>
-    )
+export default function Error() {
+  return (
+    <div className="error-page">
+      <div className="error-content">
+        <h1>😕 Oops! Página não encontrada</h1>
+        <p>A página que você tentou acessar não existe ou foi movida.</p>
+        <Link to="/" className="error-button">
+          Voltar para a Home
+        </Link>
+      </div>
+    </div>
+  );
 }
