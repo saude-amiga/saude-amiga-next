2937ed04563d2ea66a04e2fab7674dcc3a1a0add
Sabelli <sabellinho@gmail.com>
2025-11-08 01:30:56 -0300

    fix(Footer): arrumando footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; Todos os direitos reservados - Saúde Amiga {new Date().getFullYear()}</p>
+        <p>&copy; Todos os direitos reservados - Saúde Amiga - {new Date().getFullYear()}</p>
         <nav className="footer-links">
-          <a href="/">Home</a>
-          <a href="/contraste">Contraste</a>
-          <a href="/texto">Texto</a>
-          <a href="/leitor">Leitor</a>
-          <a href="/libras">Libras</a>
+          <Link to="/">Home</Link>
+          <Link to="/contraste">Contraste</Link>
+          <Link to="/texto">Texto</Link>
+          <Link to="/leitor">Leitor</Link>
+          <Link to="/libras">Libras</Link>
+          <Link to="/guias">Guias</Link>
         </nav>
       </div>
     </footer>
